3ed91a54573d145a31930e50fa38f2e11aa28a26
Silvano Cerza <silvanocerza@gmail.com>
2025-08-06 12:15:09 +0200

    Change some stuff

--- keymap.c
+++ keymap.c
@@ -2,6 +2,7 @@
 #if __has_include("keymap.h")
 #    include "keymap.h"
 #endif
+#include "gpio.h"
 
 enum layer_number {
     _QWERTY = 0,
@@ -102,6 +103,61 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        _______, _______, _______, _______, _______, _______, _______, _______
 )};
 // clang-format on
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+// SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
+// #ifdef OLED_ENABLE
+
+// oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+//     if (!is_keyboard_master()) return OLED_ROTATION_180; // flips the display 180 degrees if offhand
+//     return rotation;
+// }
+
+// // When you add source files to SRC in rules.mk, you can use functions.
+// const char *read_layer_state(void);
+// const char *read_logo(void);
+// void        set_keylog(uint16_t keycode, keyrecord_t *record);
+// const char *read_keylog(void);
+// const char *read_keylogs(void);
+
+// // const char *read_mode_icon(bool swap);
+// // const char *read_host_led_state(void);
+// // void set_timelog(void);
+// // const char *read_timelog(void);
+
+// bool oled_task_user(void) {
+//     if (is_keyboard_master()) {
+//         // If you want to change the display of OLED, you need to change here
+//         oled_write_ln(read_layer_state(), false);
+//         oled_write_ln(read_keylog(), false);
+//         oled_write_ln(read_keylogs(), false);
+//         // oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+//         // oled_write_ln(read_host_led_state(), false);
+//         // oled_write_ln(read_timelog(), false);
+//     } else {
+//         oled_write(read_logo(), false);
+//     }
+//     return false;
+// }
+// #endif // OLED_ENABLE
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     if (record->event.pressed) {
+// #ifdef OLED_ENABLE
+//         set_keylog(keycode, record);
+// #endif
+//         // set_timelog();
+//     }
+//     return true;
+// }
+
+void keyboard_pre_init_user(void) {
+    gpio_set_pin_output(24);
+    gpio_write_pin_high(24);
+}
 
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
