bbf1dea879c8906f1b343cce87b0ef122429b132
Silvano Cerza <silvanocerza@gmail.com>
2025-09-04 09:55:40 +0200

    Invert left super and control

--- keymap.c
+++ keymap.c
@@ -31,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+     * |LGUI |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+     *                   | LAlt | LCTL |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
      *                   |      |      |      |/       /         \      \ |      |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
   KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,   KC_7,   KC_8,      KC_9,     KC_0,      KC_GRV,
   KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,      KC_O,     KC_P,      KC_MINS,
-  KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,      KC_L,     KC_SCLN,   KC_QUOT,
+  KC_LGUI,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,      KC_L,     KC_SCLN,   KC_QUOT,
   KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  KC_LBRC, KC_RBRC,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSFT,
-                       KC_LALT, KC_LGUI, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC, KC_RGUI
+                       KC_LALT, KC_LCTL, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC, KC_RGUI
 ),
 
     /* LOWER
