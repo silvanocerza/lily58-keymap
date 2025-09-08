a2c77a41c02ba1c76801639c7f852bbd8d31abc5
Silvano Cerza <silvanocerza@gmail.com>
2025-09-08 12:06:41 +0200

    Invert left Control and Alt

--- keymap.c
+++ keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LAlt | LCTL |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+     *                   | LCtl | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
      *                   |      |      |      |/       /         \      \ |      |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,      KC_O,     KC_P,      KC_MINS,
   KC_LGUI,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,      KC_L,     KC_SCLN,   KC_QUOT,
   KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  KC_LBRC, KC_RBRC,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH,   KC_RSFT,
-                       KC_LALT, KC_LCTL, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC, KC_RGUI
+                       KC_LCTL, KC_LALT, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC, KC_RGUI
 ),
 
     /* LOWER
